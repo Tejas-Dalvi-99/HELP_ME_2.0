07e0729b6c9e83670e853807354e14c8e00eadc1
Tejas Dalvi <74670868+Tejas-Dalvi-99@users.noreply.github.com>
2022-12-06 17:02:20 +0530

    Create EmergencyForm.php

---
+++ EmergencyForm.php
@@ -0,0 +1,32 @@
+<?php
+
+    $NameOrPhone = $_POST['NameOrPhone']; 
+    $details = $_POST['details']; 
+    $photo = $_POST['photo'];
+
+    if (!empty($NameOrPhone) || !empty($details) || !empty($photo)) {
+        $host = "localhost" ; 
+        $dbUsername = "root" ; 
+        $dbPassword = "" ; 
+        $dbname = "login" ;
+        
+        $conn = new mysqli($host , $dbUsername , $dbPassword , $dbname) ; 
+        if(mysqli_connect_error()){
+            die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error()) ; 
+        }
+        else {
+            $INSERT = "INSERT Into emergencytable(NameOrPhone , details , photo) values( ? , ? , ?)" ; 
+                $stmt = $conn->prepare($INSERT);
+                $stmt->bind_param("sss" , $NameOrPhone , $details , $photo);  
+                $stmt->execute();
+                echo "Details Submitted Successfully !"; 
+            $stmt->close() ; 
+            $conn->close() ; 
+        }
+    }
+    else{
+        echo "Please fill all fields" ; 
+        die() ; 
+    }
+
+?>
